7f6af840000bf7736845fe42245dbaa5ecd2105c
KPW <kp@wittboldkp.de>
2022-07-16 19:23:05 +0200

    x:Name zum Menü hinzugefügt

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,26 +1,31 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Platform;
 using SDB;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
 
 namespace StS_GUI_Avalonia
 {
     public partial class MainWindow : Window
     {
         private SchulDB myschool;
+        SaveFileDialog sfd1;
         public MainWindow()
         {
             InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            myschool = new SchulDB(":memory:");
+            sfd1 = new SaveFileDialog();
             var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
             this.Icon = new WindowIcon(assets.Open(new Uri("avares://StS-GUI-Avalonia/gfx/school-building.ico")));
+            this.FindControl<MenuItem>("mnuSchuleLaden").Click += OnMnuSchoolLoad; 
         }
 
         //quelle: https://ourcodeworld.com/articles/read/471/how-to-encrypt-and-decrypt-files-using-the-aes-encryption-algorithm-in-c-sharp
@@ -170,8 +175,31 @@ namespace StS_GUI_Avalonia
                 fsCrypt.Close();
             }
         }
-        
 
+        public async void OnMnuSchoolLoad(object? sender, RoutedEventArgs e)
+        {
+            sfd1.DefaultExtension = "sqlite";
+            sfd1.Title = "Lade Datenbank";
+            List<FileDialogFilter> Filters = new();
+            FileDialogFilter filter = new();
+            List<string> extension = new()
+            {
+                "sqlite"
+            };
+            filter.Extensions = extension;
+            filter.Name = "Datenbankdatei";
+            Filters.Add(filter);
+            sfd1.Filters = Filters;
 
+            sfd1.DefaultExtension = "sqlite";
+
+            var SettingsFileName = await sfd1.ShowAsync(this);
+           Debug.WriteLine(SettingsFileName);
+        }
+
+        public void OnMnuExit(object? sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
