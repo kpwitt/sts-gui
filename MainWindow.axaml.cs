fd9b28e62b7195afd4c07c51871b25b223d3f679
KPW <kp@wittboldkp.de>
2023-03-06 21:57:23 +0100

    added static encryption methods

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -6,14 +6,17 @@ using SDB;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace StS_GUI_Avalonia
 {
     public partial class MainWindow : Window
     {
-        private SaveFileDialog sfd = new();
-        private OpenFileDialog ofd = new();
+        private SaveFileDialog gsfd = new();
+        private OpenFileDialog gofd = new();
+        private SchulDB myschool = new(":memory:");
        public MainWindow()
         {
             InitializeComponent();
@@ -21,6 +24,138 @@ namespace StS_GUI_Avalonia
             this.AttachDevTools();
 #endif
             
+        }
+       
+       //quelle: https://ourcodeworld.com/articles/read/471/how-to-encrypt-and-decrypt-files-using-the-aes-encryption-algorithm-in-c-sharp
+        /// <summary>
+        /// Creates a random salt that will be used to encrypt your file. This method is required on FileEncrypt.
+        /// </summary>
+        /// <returns></returns>
+        public static byte[] GenerateRandomSalt() {
+            byte[] data = new byte[32];
+
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create()) {
+                for (int i = 0; i < 10; i++) {
+                    // Fill the buffer with the generated data
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Decrypts an encrypted file with the FileEncrypt method through its path and the plain password.
+        /// </summary>
+        /// <param name="inputFile"></param>
+        /// <param name="outputFile"></param>
+        /// <param name="password"></param>
+        private static void FileDecrypt(string inputFile, string outputFile, string password) {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            FileStream fsCrypt = new(inputFile, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+
+            Aes AES = Aes.Create();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CFB;
+
+            CryptoStream cs = new(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fsOut = new(outputFile, FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0) {
+                    fsOut.Write(buffer, 0, read);
+                }
+            }
+            catch (CryptographicException ex_CryptographicException) {
+                Console.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            try {
+                cs.Close();
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error by closing CryptoStream: " + ex.Message);
+            }
+            finally {
+                fsOut.Close();
+                fsCrypt.Close();
+            }
+        }
+
+        /// <summary>
+        /// Encrypts a file from its path and a plain password.
+        /// </summary>
+        /// <param name="inputFile"></param>
+        /// <param name="password"></param>
+        /// <param name="outputFile"></param>
+        private static void FileEncrypt(string inputFile, string outputFile, string password) {
+            //http://stackoverflow.com/questions/27645527/aes-encryption-on-large-files
+
+            //generate random salt
+            byte[] salt = GenerateRandomSalt();
+
+            //create output file name
+            FileStream fsCrypt = new(outputFile, FileMode.Create);
+
+            //convert password string to byte arrray
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            //Set Rijndael symmetric encryption algorithm
+            Aes AES = Aes.Create();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            //http://stackoverflow.com/questions/2659214/why-do-i-need-to-use-the-rfc2898derivebytes-class-in-net-instead-of-directly
+            //"What it does is repeatedly hash the user password along with the salt." High iteration counts.
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            //Cipher modes: http://security.stackexchange.com/questions/52665/which-is-the-best-cipher-mode-and-padding-mode-for-aes-encryption
+            AES.Mode = CipherMode.CFB;
+
+            // write salt to the begining of the output file, so in this case can be random every time
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            CryptoStream cs = new(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream fsIn = new(inputFile, FileMode.Open);
+
+            //create a buffer (1mb) so only this amount will allocate in the memory and not the whole file
+            byte[] buffer = new byte[1048576];
+            int read;
+
+            try {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0) {
+                    cs.Write(buffer, 0, read);
+                }
+
+                // Close up
+                fsIn.Close();
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally {
+                cs.Close();
+                fsCrypt.Close();
+            }
         }
        private void SetupSaveDialog(SaveFileDialog sfd, string dialogtitle, string[] extensions, string[] extensionsanames)
         {
@@ -83,7 +218,7 @@ namespace StS_GUI_Avalonia
 
         public async void OnMnuSchoolLoadClick(object? sender, RoutedEventArgs e)
         {
-            SetupOpenDialog(ofd, "Lade Datenbankdatei", new String[] { "sqlite" }, new String[] { "Datenbankdatei" });
+            SetupOpenDialog(gofd, "Lade Datenbankdatei", new String[] { "sqlite" }, new String[] { "Datenbankdatei" });
 
             
         }
@@ -94,7 +229,7 @@ namespace StS_GUI_Avalonia
         }
         public async void OnMnuschulespeichernunterClick(object? sender, RoutedEventArgs e)
         {
-            SetupSaveDialog(sfd, "Lade Datenbankdatei", new String[] { "sqlite" }, new String[] { "Datenbankdatei" });
+            SetupSaveDialog(gsfd, "Lade Datenbankdatei", new String[] { "sqlite" }, new String[] { "Datenbankdatei" });
 
         }
         public void OnMnuschuleversspeichernClick(object? sender, RoutedEventArgs e)
