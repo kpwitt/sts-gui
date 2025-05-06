376b27c336a2cb37d337e6060d45ff0d94665d9d
KPW <wittbold@wittboldkp.de>
2025-05-06 08:46:26 +0200

    fix: unnötige Überprüfung auf leeren Text entfernt und durch null checks ersetzt

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -894,7 +894,7 @@ public partial class MainWindow : Window
         if (string.IsNullOrEmpty(susid) || string.IsNullOrEmpty(susvname) || string.IsNullOrEmpty(susnname) ||
             string.IsNullOrEmpty(susklasse) || string.IsNullOrEmpty(susnutzername) ||
             string.IsNullOrEmpty(suselternadresse) ||
-            susHatZweitaccount == null || string.IsNullOrEmpty(tbSuSKurse.Text) || susIstAktiv == null)
+            susHatZweitaccount == null || tbSuSKurse != null || tbSuSKurse!.Text== null|| susIstAktiv == null)
         {
             await Dispatcher.UIThread.InvokeAsync(() =>
             {
